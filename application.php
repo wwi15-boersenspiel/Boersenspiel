facd90a0698b707be269e8e90c43519baa5bcd4a
Lucas <luci8k@t-online.de>
2016-12-07 20:13:18 +0100

    //Erklärkommentare zum MVC Grundgerüst hinzugefügt

--- application.php
+++ application.php
@@ -10,57 +10,76 @@ require_once('app/controller/user.php');
 class application
 {
 
-    //Alle mögliche Pfade:
-    public static $home_index_path = 'http://193.196.168.133/home/index';
+    //Alle möglichen Pfade, zu denen ein Controller und eine Methode existiert
+    //Alle aufrufbaren Pfade müssen hier hinterlegt werden
+    //Jeder neuer Pfad wird folgendermaßen angegenben $controllername_methode_path
+    //Müssen auf dem lokalen PC angepasst werden z.B.: aus http://193.196.168.133/... muss http://localhost/.. gemacht werden
 
+    //home Controller, beinhaltet alle Starseite bezogenen Seiten
+    public static $home_index_path = 'http://193.196.168.133/home/index'; //Startseite
 
-    public static $user_index_path = 'http://193.196.168.133/user/index';
-    public static $user_login_path = 'http://193.196.168.133/user/login';
-    public static $user_register_path = 'http://193.196.168.133/user/register';
-    public static $user_logout_path = 'http://193.196.168.133/user/logout';
-    public static $user_create_path = 'http://193.196.168.133/user/create';
-    public static $user_control_path = 'http://193.196.168.133/user/control';
+    //user Controller, beinhaltet alle User bezogenen Seiten
+    public static $user_index_path = 'http://193.196.168.133/user/index'; //User Startseite
+    public static $user_login_path = 'http://193.196.168.133/user/login'; //Login Seite
+    public static $user_register_path = 'http://193.196.168.133/user/register'; //Registrierungs Seite
+    public static $user_logout_path = 'http://193.196.168.133/user/logout'; //Wird beim LogOut aufgerufen, rendert keine View sonderte leitet auf die index Seite weiter
+    public static $user_create_path = 'http://193.196.168.133/user/create'; //Wird bei der Registrations aufgerufen, rendert keine View sonderte leitet auf die index Seite weiter
+    public static $user_control_path = 'http://193.196.168.133/user/control'; //Wird beim der LogIn aufgerufen, rendert keine View sonderte leitet auf die index bzw. LogIn Seite weiter
 
+    //Methode die anhand der URL den richtigen Controller und die richtige Methode findet
+    //Wird bei jedem Seitenaufruf aufgerufen
     public function findController($url) {
 
+        //Startet Sessions, falls diese noch nicht gestartet wurden
         if (!isset($_SESSION)) session_start();
+
+        //Unterdrückt Fehler, da list() Funktion Fehler wirf falls nur Controller und keine Methode inder URL angegeben wurde
         error_reporting(E_ALL ^ E_NOTICE);
 
+
+        //fügt zur leichteren Weiterarbeit die $_GET und $_POST Parameter in einem Array zusammen
         $request = array_merge($_GET, $_POST);
-// Zuerst entfernen wir führende und abschließende Slashes.
+        // führende und abschlißende Slashes werden aus der URL entfernt und alles wird kleingeschrieben
         $url = trim(strtolower($url), '/');
-// Wir nehmen uns die ersten drei Ebenen der URL und bauen uns somit eine kleine Struktur auf
-        list($controller, $action, $id) = explode('/', $url);
-//echo $action = substr($action,0,strpos($action,"?"));
+        // Bei jedem Slash wird die URL auseinandergeschnitten und entweder der Variable $controller, $method oder $id zugeordnet
+        list($controller, $method, $id) = explode('/', $url);
+        //unnötige Zeichensätze die bei einem GET aufruf an die URL angehängt werden, werden entfernt
         $controller = (strpos($controller, "?") != false) ? substr($controller, 0, strpos($controller, "?")) : $controller;
-        $action = (strpos($action, "?") != false) ? substr($action, 0, strpos($action, "?")) : $action;
+        $method = (strpos($method, "?") != false) ? substr($method, 0, strpos($method, "?")) : $method;
         $id = (strpos($id, "?") != false) ? substr($id, 0, strpos($id, "?")) : $id;
 
-        // Wir definieren einen standard Controller, falls unsere URL leer ist.
+        //Falls kein Controller in der URL angegeben wurde, wird home als Standartcontroller definiert
         if (empty($controller)) {
             $controller = 'home';
         }
 
-// Wir definieren eine standard Action, falls unsere URL keine beinhaltet.
-        if (empty($action)) {
-            $action = 'index';
+        //Falls keine Methode in der URL angegeben wurde, wird index als Standartmethode definiert
+        if (empty($method)) {
+            $method = 'index';
         }
 
+        //Es wird überprüft ob der in der URL angegebene Controller überhaupt existiert
         if (class_exists($controller)) {
+            //Falls ja wird ein neues Objekt davon angelegt
             $controller = new $controller($request);
-
-            if (method_exists($controller, $action)) {
-                $controller->$action($id);
+            //Es wird überprüft ob die in der URL angegebene Methode überhaupt existiert
+            if (method_exists($controller, $method)) {
+                //Falls ja wird Methode ausgeführt
+                $controller->$method($id);
 
             } else {
+                //Ansonsten wird Fehlermeldung ausgegeben
                 $this->trigger_404('Die angeforderte Funktion ist nicht vorhanden');
             }
         } else {
+            //Ansonsten wird Fehlermeldung ausgegeben
             $this->trigger_404('Der angeforderte Controller ist nicht vorhanden');
         }
 
 
     }
+
+    //Function die Fehlermeldung ausgibt
 
     function trigger_404($msg = '')
     {
@@ -69,20 +88,28 @@ class application
         echo $msg;
     }
 
-    public function redirectTo($path, $flashMessage = null)
+    //Funktion die bei Aufruf auf den übergebenen Pfad weiterleitet und dabei eine Flashmessage ausgibt
+    //Sollte nur im Controller und View verwendet werden
+      public function redirectTo($path, $flashMessage = null)
     {
         $this->setFlashMessage($flashMessage);
         header("Location: " . $path);
     }
 
+    //Flash Messages:
+    //Flash Messages können im View ausgegeben werden, werden aber nach dem ausgeben wieder gelöscht
+    //Flash Messages können in folgende Kategorien gegliedert werden:
+    //success: werden im View grün hinterlegt
+    //warning: werden im View orange hinterlegt
+    //info: werden im View blau hinterlegt
+    //danger: werden im View rot hinterlegt
     public function getFlashMessage()
     {
-        // If there are any messages in the queue
         if (isset($_SESSION['flash'])) {
-            // Fetch the message queue
+
             $messages = $_SESSION['flash'];
 
-            // Empty out the message queue
+
             unset($_SESSION['flash']);
 
             return $messages;
@@ -97,6 +124,9 @@ class application
         $_SESSION['flash'] = array(type => $type, message => $message);
     }
 
+    //Liefert Username zurück falls User eingeloggt ist, sonst wird null zurückgegeben
+    //Kann im View und im Controller verwendet werden
+
     public function getCurrentUser() {
         if (isset($_SESSION["userName"])) {
             return $_SESSION["userName"];
@@ -104,6 +134,9 @@ class application
             return null;
         }
     }
+
+    //Loggt den übergegebenen User ein, falls null übergeben wird, wird aktueller User ausgeloggt
+    //Kann im View und im Controller verwendet werden
 
     public function setCurrentUser($userName) {
         if (is_null($userName)) {

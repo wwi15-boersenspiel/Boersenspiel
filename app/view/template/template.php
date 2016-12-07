e398b82c7118078da22fd8b17fea5f28b48881a7
Lucas <luci8k@t-online.de>
2016-12-07 17:14:31 +0100

    Navbar Schriftfarbe auf schwarz geändert

--- app/view/template/template.php
+++ app/view/template/template.php
@@ -25,13 +25,13 @@
     <?php $this->includeJS($render); ?>
 </head>
 <body>
-<nav>
+<nav class="light-green accent-3">
     <div class="nav-wrapper">
-        <a href="#" class="brand-logo">Logo</a>
+        <a href="#" class="brand-logo black-text">Logo</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+            <li><a class="black-text" href="sass.html">Sass</a></li>
+            <li><a class="black-text" href="badges.html">Components</a></li>
+            <li><a class="black-text" href="collapsible.html">JavaScript</a></li>
         </ul>
     </div>
 </nav>
